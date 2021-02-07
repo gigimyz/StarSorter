60c4690804ede9fdbd649c6f162fb0a6f7e110ca
Gigi Ma <mayz.gigi2001@gmail.com>
2021-02-06 18:17:36 -0800

    updated.

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,10 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+my_bot = ChatBot(name = 'PyBot', read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation','chatterbot.logic.BestMatch'])
+my_bot.get_response("Do you want to ")
+
+my_bot.get_response("Type the website you want to bookmark: ")
+
+converstation = ['']
 
